65f00a951a29b9766f6aea374fe23fa95aeefc3f
James Halliday <mail@substack.net>
2012-07-21 15:47:51 -0700

    using classes instead of ids

--- browser.js
+++ browser.js
@@ -1,19 +1,21 @@
+var questions = $('.question');
+
 function createQuestion (n) {
-    var qn = $('#question' + n);
+    var qn = $(questions[n]);
     qn.hide();
     qn.submit(function (ev) {
         ev.preventDefault();
-        var theirAnswer = parseInt(qn.find('#input' + n).val());
+        var theirAnswer = parseInt(qn.find('.answer').val());
         console.log("Their answer:" + theirAnswer);
-        var rightAnswer = eval($('#q' + n).text());
+        var rightAnswer = eval(qn.find('.q').text());
         console.log("The right answer is: "+rightAnswer);
         if (theirAnswer === rightAnswer){
             qn.hide();
-            if (n === 6) {
+            if (n === questions.length - 1) {
                 alert('All correct!');
             }
             else {
-                $('#question' + (n + 1)).show();
+                $(questions[n+1]).show();
             }
         }else{
             alert("Try again!");
@@ -21,9 +23,9 @@ function createQuestion (n) {
     });
 }
 
-// loop over the questions from 1 through 6, inclusive
-for (var i = 1; i <= 6; i++) {
+// loop over the questions from 0 through 5, inclusive
+for (var i = 0; i < 6; i++) {
     createQuestion(i);
 }
 
-$('#question1').show();
+$(questions[0]).show();
