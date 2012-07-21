5d42902b1cfcd4662a3157bd6cc5a10e2d403841
Jonathan Dinu <jondinu@gmail.com>
2012-07-21 14:26:56 -0700

    first additions

--- browser.js
+++ browser.js
@@ -1,5 +1,25 @@
-var q1 = $('#question1');
-q1.submit(function (ev) {
+var q6= $('#question6');
+q6.hide();
+var q7 = $('#question7');
+q7.hide();
+var q4 = $('#question4');
+q4.submit(function (ev) {
     ev.preventDefault();
-    console.log(q1.find('#input1').val());
+    console.log(q4.find('#input1').val());
+    q4.hide();
+    q6.show();
+});
+
+q6.submit(function (ev) {
+    ev.preventDefault();
+    console.log(q6.find('#input1').val());
+q6.hide();
+q7.show();
+});
+
+q7.submit(function (ev) {
+    ev.preventDefault();
+    console.log(q7.find('#input1').val());
+    q4.hide();
+    q6.hide();
 });
