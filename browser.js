318839ab588e99c5ae3df0518fd66ffe35832b1d
Dan Finlay <dan@danfinlay.com>
2012-07-21 13:59:08 -0700

    add a question 2

--- browser.js
+++ browser.js
@@ -1,9 +1,10 @@
 var q1 = $('#question1');
 q1.submit(function (ev) {
     ev.preventDefault();
-    var theirAnswer = q1.find('#input1').val();
+    var theirAnswer = parseInt(q1.find('#input1').val());
     console.log("Their answer:" + theirAnswer);
-    var rightAnswer = eval($('#question1'));
+    console.log($('#q1').text() )
+    var rightAnswer = parseInt(eval($('#q1').text()));
     console.log("The right answer is: "+rightAnswer);
     if (theirAnswer === rightAnswer){
     	alert("That's right!");
