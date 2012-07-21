029be375a7713e1457c9e96e394af951a4a3a9c1
James Halliday <mail@substack.net>
2012-07-21 15:20:35 -0700

    cleaned up the code and pulled from everyone

--- browser.js
+++ browser.js
@@ -1,35 +1,9 @@
-var q6= $('#question6');
-q6.hide();
-var q7 = $('#question7');
-q7.hide();
 var q1 = $('#question1');
 q1.submit(function (ev) {
     ev.preventDefault();
-<<<<<<< HEAD
-    var theirAnswer = q1.find('#input1').val();
-    console.log("Their answer:" + theirAnswer);
-    var rightAnswer = eval($('#question1'));
-    console.log("The right answer is: "+rightAnswer);
-    if (theirAnswer === rightAnswer){
-    	alert("That's right!");
-    }else{
-    	alert("Try again!");
-    }
-});
-
-q6.submit(function (ev) {
-    ev.preventDefault();
-    console.log(q6.find('#input1').val());
-q6.hide();
-q7.show();
-});
-
-=======
-
     var theirAnswer = parseInt(q1.find('#input1').val());
     console.log("Their answer:" + theirAnswer);
-    console.log($('#q1').text() )
-    var rightAnswer = parseInt(eval($('#q1').text()));
+    var rightAnswer = eval($('#q1').text());
     console.log("The right answer is: "+rightAnswer);
     if (theirAnswer === rightAnswer){
     	alert("That's right!");
@@ -37,31 +11,47 @@ q7.show();
     	alert("Try again!");
     }
 });
+
 var q2 = $('#question2');
-q2.submit(function (ev) {
-    ev.preventDefault();
+q2.submit(function () {
     var theirAnswer = parseInt(q2.find('#input2').val());
     console.log("Their answer:" + theirAnswer);
     console.log($('#q2').text() )
     var rightAnswer = parseInt(eval($('#q2').text()));
     console.log("The right answer is: "+rightAnswer);
     if (theirAnswer === rightAnswer){
+    	alert("That's right!");
+    }else{
+    	alert("Try again!");
+    }
+});
+
+var q3 = $('#question3');
+q3.submit(function (ev) {
+    ev.preventDefault();
+    var theirAnswer = parseInt(q3.find('#input3').val());
+    console.log("Their answer:" + theirAnswer);
+    console.log($('#q3').text() )
+    var rightAnswer = parseInt(eval($('#q3').text()));
+    console.log("The right answer is: "+rightAnswer);
+    if (theirAnswer === rightAnswer){
         alert("That's right!");
     }else{
         alert("Try again!");
     }
-
-
 });
 
->>>>>>> 859be1de571ac92a4b7a75cc7b5467c37d1bc5c9
-q7.submit(function (ev) {
+var q4 = $('#question4');
+q4.submit(function (ev) {
     ev.preventDefault();
-    console.log(q7.find('#input1').val());
-    q4.hide();
-    q6.hide();
-<<<<<<< HEAD
-=======
-
->>>>>>> 859be1de571ac92a4b7a75cc7b5467c37d1bc5c9
+    var theirAnswer = parseInt(q4.find('#input4').val());
+    console.log("Their answer:" + theirAnswer);
+    console.log($('#q4').text() )
+    var rightAnswer = parseInt(eval($('#q4').text()));
+    console.log("The right answer is: "+rightAnswer);
+    if (theirAnswer === rightAnswer){
+        alert("That's right!");
+    }else{
+        alert("Try again!");
+    }
 });
