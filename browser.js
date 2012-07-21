dc9f9d75122c50113df356896f4e12b376b199b8
James Halliday <mail@substack.net>
2012-07-21 15:30:30 -0700

    refactored the questions into a single function

--- browser.js
+++ browser.js
@@ -1,57 +1,22 @@
-var q1 = $('#question1');
-q1.submit(function (ev) {
-    ev.preventDefault();
-    var theirAnswer = parseInt(q1.find('#input1').val());
-    console.log("Their answer:" + theirAnswer);
-    var rightAnswer = eval($('#q1').text());
-    console.log("The right answer is: "+rightAnswer);
-    if (theirAnswer === rightAnswer){
-    	alert("That's right!");
-    }else{
-    	alert("Try again!");
-    }
-});
+function question (n) {
+    var qn = $('#question' + n);
+    qn.submit(function (ev) {
+        ev.preventDefault();
+        var theirAnswer = parseInt(qn.find('#input' + n).val());
+        console.log("Their answer:" + theirAnswer);
+        var rightAnswer = eval($('#q' + n).text());
+        console.log("The right answer is: "+rightAnswer);
+        if (theirAnswer === rightAnswer){
+            alert("That's right!");
+        }else{
+            alert("Try again!");
+        }
+    });
+}
 
-var q2 = $('#question2');
-q2.submit(function () {
-    var theirAnswer = parseInt(q2.find('#input2').val());
-    console.log("Their answer:" + theirAnswer);
-    console.log($('#q2').text() )
-    var rightAnswer = parseInt(eval($('#q2').text()));
-    console.log("The right answer is: "+rightAnswer);
-    if (theirAnswer === rightAnswer){
-    	alert("That's right!");
-    }else{
-    	alert("Try again!");
-    }
-});
-
-var q3 = $('#question3');
-q3.submit(function (ev) {
-    ev.preventDefault();
-    var theirAnswer = parseInt(q3.find('#input3').val());
-    console.log("Their answer:" + theirAnswer);
-    console.log($('#q3').text() )
-    var rightAnswer = parseInt(eval($('#q3').text()));
-    console.log("The right answer is: "+rightAnswer);
-    if (theirAnswer === rightAnswer){
-        alert("That's right!");
-    }else{
-        alert("Try again!");
-    }
-});
-
-var q4 = $('#question4');
-q4.submit(function (ev) {
-    ev.preventDefault();
-    var theirAnswer = parseInt(q4.find('#input4').val());
-    console.log("Their answer:" + theirAnswer);
-    console.log($('#q4').text() )
-    var rightAnswer = parseInt(eval($('#q4').text()));
-    console.log("The right answer is: "+rightAnswer);
-    if (theirAnswer === rightAnswer){
-        alert("That's right!");
-    }else{
-        alert("Try again!");
-    }
-});
+question(1);
+question(2);
+question(3);
+question(4);
+question(5);
+question(6);
